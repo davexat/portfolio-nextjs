0d362c2c819df0197e03daec781572a3ec55fdc5
davexat <yisuscristh20@gmail.com>
2025-08-31 01:28:31 -0500

    chore: about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import FlowBackground from '@/components/FlowBackground';
 import HeaderUI from '@/components/HeaderUI';
 import HeroUI from '@/components/HeroUI';
+import AboutUI from '@/components/AboutUI';
 
 export default function Home() {
   return (
     <>
       <HeaderUI />
-      <HeroUI />
+      {/* <HeroUI /> */}
+      <AboutUI />
     </>
   );
 }
--- src/components/HeroUI.tsx
+++ src/components/HeroUI.tsx
@@ -4,15 +4,15 @@ import { scrollToSection } from '@/lib/utils';
 
 const HeroUI = () => {
     return (
-        <section id='home' className='min-h-screen flex items-center justify-center relative overflow-hidden bg-white dark:bg-black'>
+        <section id='home' className='min-h-screen flex flex-col space-y-10 items-center justify-center relative overflow-hidden bg-white dark:bg-black'>
             {/* Content */}
-            <div className='flex flex-col items-center space-x-4 max-w-8xl mx-auto px-4'>
+            <div className='flex flex-col items-center space-y-5 max-w-8xl mx-auto px-4'>
                 <h1 className='text-7xl font-bold bg-gradient-to-r from-blue-500 via-blue-400 to-violet-600 bg-clip-text text-transparent'>David Sandoval</h1>
-                <p className='text-2xl text-slate-700 dark:text-neutral-100'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems</p>
+                <p className='text-2xl text-slate-700 dark:text-neutral-100 max-w-5xl text-center'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems.</p>
             </div>
             {/* Buttons */}
             <div className='flex items-center space-x-4'>
-                <Button onClick={() => scrollToSection('about')} className='py-2 px-3.5 bg-gradient-to-r from-blue-500/80 via-blue-400/80 to-violet-600/80 hover:bg-gray-500/70 transition-all duration-300 flex items-center space-x-5 border rounded-md overflow-hidden border-blue-100 hover:border-blue-400'>
+                <Button onClick={() => scrollToSection('about')} className='py-2 px-4 bg-gradient-to-r from-blue-500/80 via-blue-400/80 to-violet-600/80 hover:bg-gray-500/70 transition-all duration-300 flex items-center space-x-5 border rounded-md overflow-hidden border-black hover:border-blue-400 text-slate-900'>
                     <p className='text-lg'>Learn More</p>
                     <LuArrowDown className='h-5 w-5' />
                 </Button>

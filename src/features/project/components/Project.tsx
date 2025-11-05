1d2363df2adee0c80abe82241248702fc4bfaf5f
David Elias Sandoval Bernitta <yisuscristh20@gmail.com>
2025-11-05 19:27:39 +0000

    refactor: Variables CSS

--- src/features/project/components/Project.tsx
+++ src/features/project/components/Project.tsx
@@ -64,8 +64,8 @@ const projects: Project[] = [
 
 const ProjectUI = () => {
     return (
-        <section id='projects' className='p-10 gap-15 w-full flex flex-col items-center justify-center relative bg-white dark:bg-black'>
-            <h1 className='text-4xl font-bold bg-gradient-to-r from-blue-500 via-blue-400 to-violet-600 bg-clip-text text-transparent text-center'>Projects</h1>
+        <section id='projects' className='section-container'>
+            <h1 className='section-title'>Projects</h1>
             {/* Content */}
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 max-w-7xl w-full">
                 {projects.map((project, index) => (

f8aa88c870380afb6c7a95e041ec0e8510e18413
davexat <yisuscristh20@gmail.com>
2025-11-07 01:12:20 -0500

    chore: Variables CSS

--- src/features/hero/components/Hero.tsx
+++ src/features/hero/components/Hero.tsx
@@ -7,7 +7,7 @@ const HeroUI = () => {
         <section id='home' className='min-h-screen flex flex-col items-center justify-center relative overflow-hidden bg-white dark:bg-black gap-10'>
             {/* Content */}
             <div className='flex flex-col text-center items-center max-w-6xl mx-auto gap-5 px-10'>
-                <h1 className='hero-title'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems.</h1>
+                <h1 className='text-7xl font-bold bg-gradient-to-r from-blue-500 via-blue-400 to-violet-600 bg-clip-text text-transparent'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems.</h1>
             </div>
             {/* Buttons */}
             <div className='flex items-stretch gap-3'>

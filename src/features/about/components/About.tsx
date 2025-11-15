00ceef51d3af466207db60a5b44bba2913ca3ed7
davexat <yisuscristh20@gmail.com>
2025-11-14 21:00:12 -0500

    chore: About style

--- src/features/about/components/About.tsx
+++ src/features/about/components/About.tsx
@@ -3,14 +3,14 @@ export default function About() {
         <section id='about' className='layout-section bg-section'>
             <h2 className='text-title gradient-title'>About Me</h2>
             {/* Content */}
-            <div className='flex flex-col w-full xl:flex-row max-w-7xl text-xl text-justify justify-center items-center color-text gap-10'>
+            <div className='flex flex-col w-full xl:flex-row max-w-7xl text-xl text-center xl:text-justify justify-center items-center color-text gap-10'>
                 <div className="max-w-xs xl:max-w-2/5 xl:pr-15">
                     <span className="shadow-lg xl:max-w-sm items-stretch">
                         <img src='https://placehold.co/400x400' alt='Profile Picture'/>
                     </span>
                 </div>
                 <div className="flex flex-col gap-5 w-full xl:max-w-1/2">
-                    <p className=''>I'm David Sandoval, a Computer Science student at the Escuela Superior Politécnica del Litoral (ESPOL), a university internationally accredited by ABET and EUR-ACE. I'm passionate about data, artificial intelligence, and software development, and I've worked with technologies such as Python, Pandas, NumPy, Scikit-Learn, Streamlit, React, Node.js, and Django.</p>
+                    <p>I'm David Sandoval, a Computer Science student at the Escuela Superior Politécnica del Litoral (ESPOL), a university internationally accredited by ABET and EUR-ACE. I'm passionate about data, artificial intelligence, and software development, and I've worked with technologies such as Python, Pandas, NumPy, Scikit-Learn, Streamlit, React, Node.js, and Django.</p>
                     <p>I've built projects focused on applied data science, including customer churn prediction models with XGBoost, interactive dashboards in Power BI, and modern web solutions using HTML, CSS, JavaScript, and TypeScript. I also have experience creating digital portfolios, institutional landing pages, and conversational bots.</p>
                     <p>Right now, I'm working toward certifications as a Data Engineer, Data Analyst, and Data Scientist through DataCamp, and I'm also exploring Google Cloud certifications. My goal is to grow into a well-rounded data professional, combining analysis, engineering, and science to deliver innovative solutions to real-world challenges.</p>
                 </div>

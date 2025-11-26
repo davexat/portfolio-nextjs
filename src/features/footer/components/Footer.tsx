87c2cf5c5d6094cf5749fddfa5fd5252625176e0
David Elias Sandoval Bernitta <yisuscristh20@gmail.com>
2025-11-26 13:35:46 +0000

    chore: Footer section

---
+++ src/features/footer/components/Footer.tsx
@@ -0,0 +1,9 @@
+const FooterUI = () => {
+    return (
+        <footer className="w-full py-4 text-center text-sm text-gray-500">
+            © {new Date().getFullYear()} David Sandoval. All rights reserved.
+        </footer>
+    )
+};
+
+export default FooterUI;

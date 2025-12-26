fee7324b7a4bd0bdafbf77c20eb8799296aa3e1f
davexat <yisuscristh20@gmail.com>
2025-12-26 12:23:45 -0500

    refactor: hero responsive

--- src/features/hero/components/Hero.tsx
+++ src/features/hero/components/Hero.tsx
@@ -7,14 +7,14 @@ const HeroUI = ({ isDark }: { isDark: boolean }) => {
     return (
         <section id='home' className='hero'>
             <ParticlesBackground isDark={isDark} />
-            <span className='flex-center flex-col gap-10  z-1'>
-                <h1 className='gradient text-7xl text-center title-clip'>David Sandoval</h1>
+            <span className='flex-center flex-col gap-10 z-1 w-8/10'>
+                <h1 className='gradient title-hero'>David Sandoval</h1>
 
-                <p className='text-center text-2xl max-w-5xl p-2'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems.</p>
+                <p className='hero-text'>Full Stack Developer, Data Analyst & AI Enthusiast. I enjoy learning about new technologies and applying them to solve real-world problems.</p>
 
                 <div className='flex-center gap-3'>
                     <Button onClick={() => scrollToSection('#about')} className='button-hero-principal transitions'>
-                        <p className='text-xl leading-none'>Learn More</p>
+                        <p className='text-button'>Learn More</p>
                         <LuArrowDown className="size-icon" />
                     </Button>
                     <Button href='https://github.com/davexat' target='_blank' rel='noopener noreferrer' className='button-hero-icon transitions'>

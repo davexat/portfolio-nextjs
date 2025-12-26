b2b6dc19c4e63581e0ef7bd1191fddcffd89e0e6
davexat <yisuscristh20@gmail.com>
2025-12-25 23:23:38 -0500

    chore: page logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "David Sandoval - Portfolio",
   description: "My web portfolio to show my skills and projects",
+  icons: {
+    icon: '/logo.svg',
+  }
 };
 
 export default function RootLayout({

c1412969099a0133914bc5e885c85d16591a0fae
davexat <yisuscristh20@gmail.com>
2025-12-26 00:06:23 -0500

    fix: img tag

--- src/features/about/components/About.tsx
+++ src/features/about/components/About.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function About() {
     return (
         <section id='about' className='base-section'>
@@ -6,12 +8,12 @@ export default function About() {
             <div className='flex-center flex-col w-full xl:flex-row max-w-7xl text-xl text-center xl:text-justify gap-10'>
                 <div className="max-w-xs flex-center overflow-hidden xl:max-w-2/5 xl:pr-15">
                     <span className="shadow-lg rounded-full overflow-hidden xl:rounded-xl xl:max-w-sm items-stretch">
-                        <img src='/me.jpg' alt='Profile Picture' />
+                        <Image src='/me.jpg' alt='Profile Picture' width={400} height={400} />
                     </span>
                 </div>
                 <div className="flex flex-col gap-5 w-full xl:max-w-1/2">
-                    <p>I'm David Sandoval, a Computer Science student at the Escuela Superior Politécnica del Litoral (ESPOL), a university internationally accredited by ABET and EUR-ACE. I'm passionate about data, artificial intelligence, and software development, and I've worked with Pandas, NumPy, Scikit-Learn, Seaborn, Streamlit, Node.js, Django and n8n.</p>
-                    <p>Right now, I'm working toward certifications as a Data Engineer, Data Analyst, and Data Scientist through DataCamp, and I'm also exploring Google Cloud certifications. My goal is to grow into a well-rounded data professional, combining analysis, engineering, and science to deliver innovative solutions to real-world challenges.</p>
+                    <p>I&apos;m David Sandoval, a Computer Science student at the Escuela Superior Politécnica del Litoral (ESPOL), a university internationally accredited by ABET and EUR-ACE. I&apos;m passionate about data, artificial intelligence, and software development, and I&apos;ve worked with Pandas, NumPy, Scikit-Learn, Seaborn, Streamlit, Node.js, Django and n8n.</p>
+                    <p>Right now, I&apos;m working toward certifications as a Data Engineer, Data Analyst, and Data Scientist through DataCamp, and I&apos;m also exploring Google Cloud certifications. My goal is to grow into a well-rounded data professional, combining analysis, engineering, and science to deliver innovative solutions to real-world challenges.</p>
                 </div>
             </div>
         </section>
--- src/features/skill/components/tech.tsx
+++ src/features/skill/components/tech.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import React from 'react';
 import { hoverBorderColor, backgroundColor, hoverShadowColor } from "@/lib/utils";
+import Image from 'next/image';
 
 interface TechItemProps {
     icon: string;
@@ -13,7 +14,7 @@ const TechItem = ({ icon, color, text, changeMode }: TechItemProps) => {
     return (
         <li className={clsx('group relative flex-center flex-col my-container transitions', hoverBorderColor(color), hoverShadowColor(color))}>
             <span className="relative group-hover:-translate-y-8 transitions">
-                <img src={icon} alt={text} className={clsx('h-14 w-14', changeMode && 'invert')} />
+                <Image src={icon} alt={text} width={56} height={56} className={clsx('h-14 w-14', changeMode && 'invert')} />
                 <span className={clsx("absolute inset-0 rounded-md opacity-0 group-hover:opacity-30 blur-md -z-1 transitions", backgroundColor(color))}></span>
             </span>
             <p className='opacity-0 w-full text-center group-hover:opacity-80 absolute top-15 text-sm transitions color-text'>{text}</p>
